a0c7c48b728ac696a4df3d283d01912a06834094
himynameisdave <d@velunny.com>
2017-05-06 14:06:54 -0700

    Use path module to (hopefully) help Heroku find that file. Also a postinstall build might help :P

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use('/', express.static('./dist'));
+app.use('/', express.static(path.resolve(process.cwd(), './dist')));
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`); // eslint-disable-line no-console
 });
