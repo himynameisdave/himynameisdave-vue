71ce2d11a286e9646ad90d6aa7d444166f1a9f3d
himynameisdave <d@velunny.com>
2017-05-06 10:35:47 -0700

    Adds a Name component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-  </div>
+    <main id="app">
+        <Face />
+        <Name />
+    </main>
 </template>
 
 <script>
-// import Hello from './components/Hello';
+import Face from './components/Face';
+import Name from './components/Name';
 
 export default {
-  name: 'app',
-  components: {
-    // Hello,
-  },
+    name: 'app',
+    components: {
+        Face,
+        Name,
+    },
 };
 </script>
 
 <style>
+html, body {
+    display: block;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    background-color: #E9EFF2;
+    color: #465F6E;
+    align-items: center;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    padding-top: 80px;
+    height: 100%;
+    width: 100%;
 }
 </style>
---
+++ src/components/Name.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 class="name">Dave Lunny</h1>
+</template>
+
+
+<script>
+export default {
+    name: 'Name',
+};
+</script>
+
+
+<style scoped>
+.name {
+    font-size: 3.75rem;
+    font-weight: 300;
+    margin: 0 0 30px;
+    letter-spacing: -0.07rem;
+    text-transform: lowercase;
+}
+</style>
