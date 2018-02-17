42e057906e91cf87e7964d4c34c705746ba663e5
himynameisdave <d@velunny.com>
2018-02-17 13:37:55 -0800

    📦 Simplifies build config

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-
-const app = express();
-app.use('/', express.static(path.resolve(process.cwd(), './dist')));
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`); // eslint-disable-line no-console
-});
--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
+import App from './App.vue';
 
 const files = require.context('!svg-sprite-loader!./assets/icons', false, /.*\.svg$/);
 files.keys().forEach(files);
-
-Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
